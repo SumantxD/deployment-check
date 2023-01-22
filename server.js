9f4d82f521193d23af399ffb4e2938d48cafb516
Sumant.xD <st72365@gmail.com>
2023-01-22 14:53:28 +0530

    earth added

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/earth', (req, res) => {
+    res.send('Wrlcome to earth');
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
